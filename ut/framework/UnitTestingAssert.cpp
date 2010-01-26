a0ba7e4fb40c42dc9a5beeb435c2ccd1cf390e84
torshie <torshie@49556248-bd73-11de-827c-f3460f6bf3f5>
2010-01-26 10:28:06 +0000

    Save

--- ut/framework/UnitTestingAssert.cpp
+++ ut/framework/UnitTestingAssert.cpp
@@ -1,11 +1,15 @@
 #include "UnitTestingAssert.h"
-#include "TestRunner.h"
 
 namespace kernel {
 
 void UnitTestingAssert::assert(bool value, const char* file, int line,
 		const char* expression) {
 	TestRunner& runner = getSingleInstance<TestRunner>();
+	if (value) {
+		runner.assertionFailed(file, line, expression);
+	} else {
+		runner.assertionSucceeded();
+	}
 }
 
 } /* namespace kernel */
