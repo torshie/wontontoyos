bb0591ffbdf764abed0d107b5713973b3c036e12
torshie <torshie@49556248-bd73-11de-827c-f3460f6bf3f5>
2010-01-27 14:59:38 +0000

    Simple unit testing framework runs!

--- src/ut/framework/UnitTestingAssert.cpp
+++ src/ut/framework/UnitTestingAssert.cpp
@@ -6,9 +6,9 @@ void UnitTestingAssert::assert(bool value, const char* file, int line,
 		const char* expression) {
 	TestRunner& runner = getSingleInstance<TestRunner>();
 	if (value) {
-		runner.assertionFailed(file, line, expression);
-	} else {
 		runner.assertionSucceeded();
+	} else {
+		runner.assertionFailed(file, line, expression);
 	}
 }
 
