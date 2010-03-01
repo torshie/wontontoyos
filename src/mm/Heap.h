0124bb4456a9a77f3f8e4cb259b7f197ed8da361
torshie <torshie@49556248-bd73-11de-827c-f3460f6bf3f5>
2010-03-01 10:55:46 +0000

    Begin to implement data structures used for memory allocation.

---
+++ src/mm/Heap.h
@@ -0,0 +1,13 @@
+#ifndef KERNEL_MM_HEAP_H_INCLUDED
+#define KERNEL_MM_HEAP_H_INCLUDED
+
+namespace kernel {
+
+template<typename Load, typename Allocator> class Heap {
+public:
+
+};
+
+} // namespace kernel
+
+#endif // KERNEL_MM_HEAP_H_INCLUDED
